604a2a4918c7129a5d809ab5d42f0c660317f798
Christine Dodrill <me@christine.website>
2017-05-11 01:46:36 -0700

    split this up more

--- build/aura.rb
+++ build/aura.rb
@@ -2,15 +2,15 @@ import "build/prelude.rb"
 
 gobuild "cmd/aura"
 
-run %q[ apk add --virtual streamripper-deps --no-cache wget build-base glib-dev \
-     && mkdir /tmp/streamripper \
-     && cd /tmp/streamripper \
+run %q[ apk add --virtual streamripper-deps --no-cache wget build-base glib-dev ]
+run "mkdir /tmp/streamripper"
+run %q[ cd /tmp/streamripper \
      && wget https://xena.greedo.xeserv.us/files/streamripper.tgz \
      && tar zxf ./streamripper.tgz \
      && cd streamripper-1.64.6 \
      && ./configure && make -j && chmod +x install-sh && make install \
-     && rm -rf /tmp/streamripper \
-     && apk del streamripper-deps && apk add --no-cache glib ]
+     && rm -rf /tmp/streamripper ]
+run "apk del streamripper-deps && apk add --no-cache glib"
 
 cleanup
 cmd "/go/src/github.com/PonyvilleFM/aura/run/aura.sh"
