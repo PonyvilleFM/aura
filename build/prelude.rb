7d2e039df6d3022bfea4e1aad3e049e55f2f1eba
Christine Dodrill <me@christine.website>
2017-11-26 16:25:48 -0800

    build: go 1.9.2

--- build/prelude.rb
+++ build/prelude.rb
@@ -1,6 +1,6 @@
-from "xena/go-mini:1.9.1"
+from "xena/go-mini:1.9.2"
 
-run "go1.9.1 download"
+run "go1.9.2 download"
 
 $repo = "github.com/PonyvilleFM/aura"
 
@@ -9,12 +9,12 @@ def foldercopy(dir)
 end
 
 def gobuild(pkg)
-  run "go1.9.1 build #{$repo}/#{pkg} && go1.9.1 install #{$repo}/#{pkg}"
+  run "go1.9.2 build #{$repo}/#{pkg} && go1.9.2 install #{$repo}/#{pkg}"
 end
 
 def cleanup()
   run "rm -rf /root/sdk /root/go/pkg /usr/local/go"
-  run "apk del go1.9.1"
+  run "apk del go1.9.2"
   flatten
 end
 
