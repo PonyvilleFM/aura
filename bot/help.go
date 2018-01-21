c00b35d8c4ef6b99527443c12bab2e0f52b3a9dd
George Warden <georgwarden@gmail.com>
2018-01-21 22:29:46 +1000

    Added command `;dhelp` that sends the result of `;help` through DM

--- bot/help.go
+++ bot/help.go
@@ -10,13 +10,7 @@ func (cs *CommandSet) help(s *discordgo.Session, m *discordgo.Message, parv []st
 	switch len(parv) {
 	case 1:
 		// print all help on all commands
-		result := "Bot commands: \n"
-
-		for verb, cmd := range cs.cmds {
-			result += fmt.Sprintf("%s%s: %s\n", cs.Prefix, verb, cmd.Helptext())
-		}
-
-		result += "If there's any problems please don't hesitate to ask a server admin for help."
+		result := formHelp()
 
 		s.ChannelMessageSend(m.ChannelID, result)
 
@@ -25,4 +19,32 @@ func (cs *CommandSet) help(s *discordgo.Session, m *discordgo.Message, parv []st
 	}
 
 	return nil
+}
+
+func (cs *CommandSet) dhelp(s *discordgo.Session, m *discordgo.Message, parv []string) error {
+	switch len(parv) {
+	case 1:
+
+		result := formHelp()
+
+		authorChannel, err := s.UserChannelCreate(m.Author.ID)
+		s.ChannelMessageSend(authorChannel.ID, result)
+
+		s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("@%s check direct messages, help is there!", m.Author.Username))
+
+	default:
+		return ErrParvCountMismatch
+	}
+
+	return nil
+}
+
+func formHelp() string {
+	result := "Bot commands: \n"
+
+	for verb, cmd := range cs.cmds {
+		result += fmt.Sprintf("%s%s: %s\n", cs.Prefix, verb, cmd.Helptext())
+	}
+
+	return (result + "If there's any problems please don't hesitate to ask a server admin for help.")
 }
