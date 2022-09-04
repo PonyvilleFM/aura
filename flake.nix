408e496c137a2a798eec4cfc6f0d69f4c2f1f2c8
Xe <me@christine.website>
2022-09-04 16:53:14 +0000

    update discordgo
    Signed-off-by: Xe <me@christine.website>

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "A basic Go web server setup";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachSystem [
+      "x86_64-linux"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+    ] (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        version = builtins.substring 0 8 self.lastModifiedDate;
+      in {
+        packages = {
+          default = pkgs.buildGoModule {
+            pname = "web-server";
+            inherit version;
+            src = ./.;
+
+            # This hash locks the dependencies of this package. It is
+            # necessary because of how Go requires network access to resolve
+            # VCS.  See https://www.tweag.io/blog/2021-03-04-gomod2nix/ for
+            # details. Normally one can build with a fake sha256 and rely on native Go
+            # mechanisms to tell you what the hash should be or determine what
+            # it should be "out-of-band" with other tooling (eg. gomod2nix).
+            # To begin with it is recommended to set this, but one must
+            # remeber to bump this hash when your dependencies change.
+            #vendorSha256 = pkgs.lib.fakeSha256;
+            vendorSha256 =
+              "sha256-pQpattmS9VmO3ZIQUFn66az8GSmB4IvYhTTCFn6SUmo=";
+          };
+        };
+
+        defaultApp = utils.lib.mkApp { drv = self.packages.${system}.default; };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [ go gopls gotools go-tools ];
+        };
+      });
+}
--- go.mod
+++ go.mod
@@ -5,7 +5,7 @@ go 1.13
 require (
 	github.com/Pallinder/go-randomdata v0.0.0-20170410161340-8c3362a5e678
 	github.com/Sirupsen/logrus v1.0.4-0.20170822132746-89742aefa4b2
-	github.com/bwmarrin/discordgo v0.20.2
+	github.com/bwmarrin/discordgo v0.26.1
 	github.com/cenkalti/backoff v1.1.0 // indirect
 	github.com/dghubble/go-twitter v0.0.0-20170910035229-c4115fa44a92
 	github.com/dghubble/oauth1 v0.4.1-0.20170516172431-7d51c10e15ca
@@ -22,8 +22,6 @@ require (
 	github.com/speps/go-hashids v1.0.1-0.20170705204438-c6ced3330f13
 	github.com/stretchr/testify v1.4.0 // indirect
 	github.com/tebeka/strftime v0.0.0-20140926081919-3f9c7761e312
-	golang.org/x/crypto v0.0.0-20200320181102-891825fb96df // indirect
-	golang.org/x/sys v0.0.0-20200321134203-328b4cd54aae // indirect
 	golang.org/x/time v0.0.0-20170927054726-6dc17368e09b
 	gopkg.in/airbrake/gobrake.v2 v2.0.9 // indirect
 	gopkg.in/gemnasium/logrus-airbrake-hook.v2 v2.1.2 // indirect
